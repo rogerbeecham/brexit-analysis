f7e6b6100a2a70d2c4912edcafef1e05743b9f69
rogerbeecham <roger.beecham@city.ac.uk>
2017-04-28 12:49:35 +0100

    Tidy bootstrap function.

--- src/do_bootstrap_models.R
+++ src/do_bootstrap_models.R
@@ -38,8 +38,8 @@ do_bootstrap_superregion <- function(data, superregion, cats) {
               lower=quantile(value,0.05, na.rm = TRUE),
               upper=quantile(value,0.95, na.rm = TRUE)) %>%
                 filter(row!="(Intercept)")
-  cv_fit_lasso <- cv.glmnet(as.matrix(data %>% select(younger_adults:eu_born)),  as.matrix(data %>% select(Leave)), alpha=1) 
-  observed <- as_tibble(tidy(coef(cv_fit_lasso, s = "lambda.1se"))) %>%
+                cv_fit_lasso <- glmnet::cv.glmnet(as.matrix(data %>% select(younger_adults:eu_born)),  as.matrix(data %>% select(Leave)), alpha=1)
+  observed <- as_tibble(tidy(glmnet::coef(cv_fit_lasso, s = "lambda.1se"))) %>%
                 mutate(observed=value) %>%
                   select(row, observed) %>%
                     filter(row!="(Intercept)")
@@ -48,39 +48,3 @@ do_bootstrap_superregion <- function(data, superregion, cats) {
               add_variables(.,cats)
   return(models)
 }
-
-
-
-
-models_east <- resamples_east %>% 
-  mutate(model = purrr::map(strap,do_lasso_super_region)) 
-
-
-xvars_east <- data_gb@data %>% filter(super_region=="East") 
-%>% select(younger_adults:eu_born)
-x_east <- as.matrix(data_gb@data %>% filter(super_region=="East") %>% select(younger_adults:eu_born))
-y_east <- as.matrix(data_gb@data %>% filter(super_region=="East") %>% select(Leave)) 
-
-fit_east <- glmnet(x_east,y_east, alpha=1)
-cv_fit_east <- cv.glmnet(x_east,y_east, alpha=1)
-
-xvars_east <- xvars_east %>% 
-  mutate(
-    fitted =  as.numeric(predict.glmnet(fit_east, newx=x_east, s =  cv_fit_east$lambda.1se)),
-    obs = as.numeric(y_east),
-    resid = obs-fitted
-  )
-
-r2_east <- (xvars_east %>%
-              select(obs,fitted) %>%
-              boot::corr())^2
-
-east_lasso <- do_lasso(y_east, x_east)
-
-observed_east <- as_tibble(tidy(coef(east_lasso, s = "lambda.1se"))) %>%
-  mutate(observed=value) %>%
-  select(row, observed) %>%
-  filter(row!="(Intercept)")
-
-boot_lasso_east <- left_join(observed_east,boot_lasso_east) 
-boot_lasso_east$superregion <- rep("East",nrow(boot_lasso_east))
